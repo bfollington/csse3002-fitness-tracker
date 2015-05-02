91f8f13248560eb2de9bacfbd7439637dae3d5ab
Max Koopman <m_koopman@me.com>
2015-05-02 16:11:53 +1000

    Can now push run objects and retrieve the latest run
    ID is left blank until the object is pushed to the server.
    Retrieved runs have the ID populated with the mongo ID

--- webapp/db.py
+++ webapp/db.py
@@ -7,6 +7,10 @@ class Waypoint:
         self.lat = lat
         self.lon = lon
 
+    @staticmethod
+    def from_mongo_obj( obj ):
+        return Waypoint( obj[ u'time' ], obj[ u'lat' ], obj[ u'lon' ] )
+
     def to_dict( self ):
         return {
             "time": self.time,
@@ -25,6 +29,21 @@ class Run:
         self.start_time = self.waypoints[ 0 ].time
         self.end_time = self.waypoints[ -1 ].time
 
+        # Does not get an ID until it is pushed to the database
+        self._id = ""
+
+    @staticmethod
+    def from_mongo_obj( obj ):
+        wps = []
+        for wp in obj[ u'waypoints' ]:
+            wps.append( Waypoint.from_mongo_obj( wp ) )
+
+        run = Run( wps )
+        run._id = obj[ u'_id' ]
+
+        return run
+
+
     def to_dict( self ):
         return {
             "start_time": self.start_time,
@@ -38,16 +57,34 @@ class RunDatabase:
         # Hard coded to development for now
         self.db = self.client.fitness_tracker_development
 
+    def push_run( self, run ):
+        run.id = self.db.runs.insert_one( run.to_dict() ).inserted_id
+        return run.id
+
     def demo_insert( self ):
         wps = []
         wps.append( Waypoint( 1, 5, 2 ) )
         wps.append( Waypoint( 2, 6, 3 ) )
         wps.append( Waypoint( 3, 7, 4 ) )
-
         run = Run( wps )
 
-        self.db.runs.insert_one( run.to_dict() )
+        self.push_run( run )
+
+
+        wps = []
+        wps.append( Waypoint( 4, 5, 2 ) )
+        wps.append( Waypoint( 5, 6, 3 ) )
+        wps.append( Waypoint( 6, 7, 4 ) )
+        run = Run( wps )
+
+        self.push_run( run )
+
+    def get_latest_run( self ):
+        run = self.db.runs.find_one( sort = [ ( "end_time", -1 ) ] )
+
+        return Run.from_mongo_obj( run )
 
 if ( __name__ == "__main__" ):
     db = RunDatabase()
-    db.demo_insert()
+    #db.demo_insert()
+    print db.get_latest_run().to_dict()
