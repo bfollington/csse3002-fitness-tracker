08699e91351463b08d7814ad21159b8dd5e5c41a
Max Koopman <m_koopman@me.com>
2015-05-02 16:34:41 +1000

    Finished implementation of the database, with basic usage examples

--- webapp/db.py
+++ webapp/db.py
@@ -1,5 +1,6 @@
 import pymongo as mongo
 
+from bson.objectid import ObjectId
 
 class Waypoint:
     def __init__( self, time, lat, lon ):
@@ -39,10 +40,9 @@ class Run:
             wps.append( Waypoint.from_mongo_obj( wp ) )
 
         run = Run( wps )
-        run._id = obj[ u'_id' ]
+        run._id = str( obj[ u'_id' ] )
 
         return run
-
 
     def to_dict( self ):
         return {
@@ -51,40 +51,87 @@ class Run:
             "waypoints": [ waypoint.to_dict() for waypoint in self.waypoints ]
         }
 
+
+
 class RunDatabase:
     def __init__( self ):
         self.client = mongo.MongoClient()
         # Hard coded to development for now
         self.db = self.client.fitness_tracker_development
 
+    def clear_runs( self ):
+        self.db.runs.remove()
+
     def push_run( self, run ):
         run.id = self.db.runs.insert_one( run.to_dict() ).inserted_id
         return run.id
 
-    def demo_insert( self ):
-        wps = []
-        wps.append( Waypoint( 1, 5, 2 ) )
-        wps.append( Waypoint( 2, 6, 3 ) )
-        wps.append( Waypoint( 3, 7, 4 ) )
-        run = Run( wps )
-
-        self.push_run( run )
-
-
-        wps = []
-        wps.append( Waypoint( 4, 5, 2 ) )
-        wps.append( Waypoint( 5, 6, 3 ) )
-        wps.append( Waypoint( 6, 7, 4 ) )
-        run = Run( wps )
-
-        self.push_run( run )
 
     def get_latest_run( self ):
         run = self.db.runs.find_one( sort = [ ( "end_time", -1 ) ] )
 
         return Run.from_mongo_obj( run )
 
+    def get_run_list( self ):
+        runs = self.db.runs.find( {}, { "start_time": 1, "end_time": 1 } )
+
+        def parse_mongo_obj( obj ):
+            return {
+                "_id": obj[ u'_id' ],
+                "start_time": obj[ u'start_time' ],
+                "end_time": obj[ u'end_time' ]
+            }
+
+        return [ parse_mongo_obj( run ) for run in runs ]
+
+    def get_waypoints_for( self, id ):
+        wps = self.db.runs.find_one( {'_id': ObjectId( id ) }, { 'waypoints': 1 } )[ u'waypoints' ]
+        return [ Waypoint.from_mongo_obj( wp ) for wp in wps ]
+
+
+def demo_insert( db ):
+    wps = []
+    wps.append( Waypoint( 1, 5, 2 ) )
+    wps.append( Waypoint( 2, 6, 3 ) )
+    wps.append( Waypoint( 3, 7, 4 ) )
+    run = Run( wps )
+
+    db.push_run( run )
+
+    wps = []
+    wps.append( Waypoint( 4, 5, 2 ) )
+    wps.append( Waypoint( 5, 6, 3 ) )
+    wps.append( Waypoint( 6, 7, 4 ) )
+    run = Run( wps )
+
+    db.push_run( run )
+
+# Simple test which clears the database, then runs some simple tests
 if ( __name__ == "__main__" ):
+    # Create a test database
     db = RunDatabase()
-    #db.demo_insert()
-    print db.get_latest_run().to_dict()
+
+    # Clear the runs database, if it exists
+    db.clear_runs()
+
+    # Insert some dummy data
+    demo_insert( db )
+
+    # Retrieve the latest run, and print it as a dict
+    print "Testing get_latest_run"
+    latest_run = db.get_latest_run()
+    print latest_run.to_dict()
+    print ""
+
+    # Retrieve all of the runs and print them
+    runs = db.get_run_list()
+    print "Testing get_run_list"
+    print runs
+    print ""
+
+    # Get the waypoints for the latest run
+    last_run_id = ObjectId( latest_run._id )
+    print "Testing get_waypoints_for"
+    # Print them as dicts, not waypoint objects
+    print [ wp.to_dict() for wp in db.get_waypoints_for( last_run_id ) ]
+
