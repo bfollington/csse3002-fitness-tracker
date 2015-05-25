ef89c1ba66e6cc987356b518ad3ff4cca0558c70
Ben Follington <bfollington@gmail.com>
2015-05-25 10:33:03 +1000

    Update all charts to follow the same updatable format

--- webapp/src/jsx/components/LineChart.jsx
+++ webapp/src/jsx/components/LineChart.jsx
@@ -5,20 +5,18 @@ export class LineChart extends React.Component {
         this.chart = null;
     }
 
-    componentDidMount() {
+    createChart() {
         var context = $(React.findDOMNode(this)).find(".chart")[0].getContext("2d");
+        this.chart = new ChartJs(context).Line(this.props.data, this.props.opts);
+    }
 
-
-        this.chart = new ChartJs(context).Line(this.props.data, {});
+    componentDidMount() {
+        this.createChart();
     }
 
     componentWillUpdate() {
-        console.log("Test");
         this.chart.destroy();
-
-        var context = $(React.findDOMNode(this)).find(".chart")[0].getContext("2d");
-
-        var chart = new ChartJs(context).Line(this.props.data, this.props.opts);
+        this.createChart();
     }
 
     render() {
