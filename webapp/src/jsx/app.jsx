259034363ce369f5b69607d38aa1648163021037
Max Koopman <m_koopman@me.com>
2015-06-03 14:08:23 +1000

    Commented pages and some components within the web application

--- webapp/src/jsx/app.jsx
+++ webapp/src/jsx/app.jsx
@@ -8,12 +8,16 @@ import {RunDataController} from './pages/controllers/RunDataController.jsx';
 var moment = require("moment");
 
 (function() {
+    /* A global object to hold utilities. */
     window.app = {};
 
+    /* Expose the moment module. */
     window.app.moment = moment;
+    /* Specify the global formats for time and date. */
     window.app.dayFormat = "dddd, MMM Do YYYY";
     window.app.timeFormat = "h:mm:ss a";
 
+    /* Specify the routes for each screen. */
     window.app.router = new Router('mount');
     var router = window.app.router;
 
